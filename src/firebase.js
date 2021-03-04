1d9e7da5d98b3c913c79b9bb41f0dd4ff85e86b5
Mert Karaca <38283518+sjuhyeon@users.noreply.github.com>
2021-03-05 08:59:32 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,11 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBIi8JPPjQgOll0q1Ur9F_bDgQff3YEW_I",
-    authDomain: "slack-clone-test-d9ba6.firebaseapp.com",
-    projectId: "slack-clone-test-d9ba6",
-    storageBucket: "slack-clone-test-d9ba6.appspot.com",
-    messagingSenderId: "36903566306",
-    appId: "1:36903566306:web:e80c07481f8b1adfc497bb",
-    measurementId: "G-89D64DDXZV"
+    /*
+    
+    FIRE BASE CONFIGS
+    
+    */
 };
 
 const fireBaseApp = firebase.initializeApp(firebaseConfig);
